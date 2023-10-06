d84cf506ff3979d9c3c580e2107f3c892b07d629
Taki Enari <taqi.enari@rakuten.com>
2023-10-06 15:11:05 +0900

    Add comment

--- app/src/main/java/com/example/lazycolumnbugsample/MainActivity.kt
+++ app/src/main/java/com/example/lazycolumnbugsample/MainActivity.kt
@@ -1,29 +1,15 @@
 package com.example.lazycolumnbugsample
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 import androidx.fragment.app.commit
 import androidx.navigation.fragment.NavHostFragment
-import com.example.lazycolumnbugsample.ui.theme.LazyColumnBugSampleTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowCompat.setDecorFitsSystemWindows(window, false) // Setting this to true alleviates the lazy column issue but app can no longer support edge to edge.
 
         setContentView(R.layout.activity_main)
         val navHostFragment = NavHostFragment.create(R.navigation.app_navigation)
